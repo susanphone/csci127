90cdf2833a39f9dab8296e3a027a25a58e455cdb
Susan McCartney <susan.a.mccartney12@gmail.com>
2020-04-23 13:48:58 -0600

    lab12

--- Labs/Lab12/Susan-McCartney-Lab12.py
+++ Labs/Lab12/Susan-McCartney-Lab12.py
@@ -10,22 +10,35 @@ from matplotlib import interactive
 
 def process(file_name):
     # read the file_name into a pandas dataframe
-    file = pd.read_csv(file_name)
-    print(file)
-    data = pd.DataFrame(file_name, columns= pd.DataFrame(columns))
-    print(data)
+    df = pd.read_csv(file_name, header=0)
+    # print(df)
+    dataset = pd.DataFrame(data=df, index=None)
+    print(dataset)
+
+
+
+    # data = pd.DataFrame(file_name)
+    # print(data)
     
     #labels found from DataFrame.column on source-----column headers
     #use lesson1.py
     # sort and plot the dataframe using arguments "title", "legend", "x", "y", and "kind"
-
+    data_chart = pd.Series(['College Enrollment'], ['Years at MSU'])
     #which title to sort
+    if data_chart == 'College Enrollemnt':
+        x_axis = 'College Enrollment'
+        y_axis = 'College Name'
+        plt.title = 'fig1'
+    if data_chart == 'Years at MSU':
+        x_axis = 'CS Professor'
+        y_axis = 'Years at MSU'
+        plt.title = 'fig2'
     # # The only statements that may use the matplotlib library appear next.
-
-    # # Do not modify them.
-    # plt.xlabel(x_axis)      # Note: x-axis should be determined above
-    # plt.ylabel(y_axis)      # Note: y-axis should be determined above
-    # plt.show()
+    # data_chart.plot(x= x_axis, y= y_axis, kind='bar')
+    # Do not modify them.
+    plt.xlabel(x_axis)      # Note: x-axis should be determined above
+    plt.ylabel(y_axis)      # Note: y-axis should be determined above
+    plt.show()
 
 # -------------------------------------------------
 
